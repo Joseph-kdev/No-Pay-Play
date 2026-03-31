ef9fbf4080f405c0182b9effa93ce38d4f3f3a33
Joseph-kdev <cjkinuthia5@gmail.com>
2026-03-31 18:15:10 +0300

    feat: fix sqlite creation for vercel. Add requests library to requirements

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import os
 from flask import Flask
 from blueprint.home.home import home
 from blueprint.home.home import db
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///games.db"
+app.secret_key = os.environ.get("SECRET_KEY", "fallback_secret_key")
+
+if os.environ.get("VERCEL") == "1":
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/games.db"
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///games.db"
 
 #change the stuff b4 committing
 db.init_app(app)
